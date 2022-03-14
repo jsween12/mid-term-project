5f2479c0e62643064ed1ba04093ef5c3e07ca0bd
Jonathan Sweeney <greatoutdoorskc@gmail.com>
2022-03-14 18:58:27 -0500

    updated database connection for Heroku

--- config/Database.php
+++ config/Database.php
@@ -1,10 +1,13 @@
+
 <?php
+
+/*
     class Database {
         //Database parameters
         private $host = "x8autxobia7sgh74.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         private $db_name = 'cvjc3r90rtdy4yc5'; 
         private $username = 'qm7c0wjqg3rep2tu'; 
-        private $password = getenv("pass") ; 
+        private $password = 'wtkltqyyhjgilte5 '; 
         private $conn; 
 
         // Database connection with pdo
@@ -15,12 +18,61 @@
             try{
                 $this->conn = new PDO('mysql: host='. $this->host . ';dbname='. $this->db_name, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+                echo "Connection successful"; 
             } catch (PDOException $e) {
-                echo 'Connection Error: '. $e->getMessage(); 
+                echo 'Connection Failed: '. $e->getMessage(); 
             }
             return $this->conn; 
         }
 
     }
+    */
+
+    //lets try this again for Heroku...
+
+    class Database{
+        private conn; 
+
+        public function connect(){
+            //get the url from config vars on Heroku
+            $url = getenv('JAWSDB_URL');
+            //it looks like this uses the url to assign all the following variables 
+            $dbparts = parse_url($url); 
+
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass']; 
+            $database = ltrim($dbparts['path'], '/'); 
+
+            try{
+
+            $conn = new PDO("mysql:host=" . $hostname . "; dbname=" . $database.", " . $username. ', ' . $password); 
+
+            $conn->setAttribute(PDO::ATR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connection Successful";
+            }
+            catch(PDOException $e)
+            {
+                echo "Connection failed: " . $e->getMessage(); 
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
 
     ?>
+    
