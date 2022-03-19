84e10eb10589f59d8751184b278658ce6e8ef98e
Jonathan Sweeney <greatoutdoorskc@gmail.com>
2022-03-18 20:12:08 -0500

    around and around we go

--- config/Database.php
+++ config/Database.php
@@ -2,19 +2,24 @@
 class Database {
   // Define the class properties here
 private $conn; 
+private $hostname; 
+private $username; 
+private $password; 
+private $database; 
+
   public function connect() {
     // if creating a Heroku connection, this is straight from the dev center link: 
     $url = getenv('JAWSDB_URL');
     $dbparts = parse_url($url);
 
-    $hostname = $dbparts['host'];
-    $username = $dbparts['user'];
-    $password = $dbparts['pass'];
-    $database = ltrim($dbparts['path'],'/');
+    $this->hostname = $dbparts['host'];
+    $this->username = $dbparts['user'];
+    $this->password = $dbparts['pass'];
+    $this->database = ltrim($dbparts['path'],'/');
 
     // Create your new PDO connection 
     try {
-      $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+      $this->conn = new PDO("mysql:host=$this->hostname;dbname=$this->database", $this->username, $this->password);
       // set the PDO error mode to exception
       $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       echo "Connected successfully";
